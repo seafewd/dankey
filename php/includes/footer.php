4d6aa40e0e6ef274274f43ef8409e97a0e8ecef0
Alexander Korpas <alexander@alexanders-air.bfh.ch>
2017-10-18 14:34:06 +0200

    migrated local html to php files

---
+++ php/includes/footer.php
@@ -0,0 +1,1 @@
+<h1>Dankey's TecShop &copy; 2017</h1>
---
+++ php/includes/header.php
@@ -0,0 +1,22 @@
+<header>
+  <div class="container">
+    <div id="utility_header">
+      <h1 class="companyHeader"><span class="highlight">Dankey's</span> TecShop</h1>
+
+      <div id="searchBar">
+      <form>
+        <input type="email" placeholder="Search for a product...">
+        <button type="submit" class="searchBar_button">Search</button>
+      </form>
+    </div>
+    <nav>
+      <ul>
+        <li><a href="index.html">Contact</a></li>
+        <li><a href="index.html">Help</a></li>
+        <li><a href="index.html">About us</a></li>
+        <li><a href="index.html">Social Media</a></li>
+      </ul>
+    </nav>
+  </div>
+  </div>
+</header>
