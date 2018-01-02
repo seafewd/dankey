9ac05736133830dd5a958e7f02fc628195ee7313
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-02 22:32:18 +0100

    asd

--- public/products/graphics_cards.php
+++ public/products/graphics_cards.php
@@ -5,7 +5,8 @@ require_once ( ABS_FILE . '/php/includes/splash_image_box.php' );
 require_once ( ABS_FILE . '/php/includes/article_main_outer.php' );
 
 $name = str_replace('_', ' ', $_GET['produdct']);
-echo $name;
+
+echo "<script type='text/javascript'>alert('$name');</script>"
 
 $pdo = new PDO('mysql:host=localhost;dbname=dankeyswebshop', 'dankey', 'J2DGi7Ql#XG&u^');
 $statement = $pdo->prepare("SELECT * FROM graphics_cards WHERE name = :name");
