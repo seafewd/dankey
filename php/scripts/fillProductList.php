c7794d3a9dcf86299e40e01e2896171e93805099
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-02 22:02:31 +0100

    asd

--- php/scripts/fillProductList.php
+++ php/scripts/fillProductList.php
@@ -15,13 +15,14 @@ $product_list = $statement->fetchAll();
 
 <ul id="productList">
 <?php foreach($product_list as $product) { ?>
+  <?php $name = str_replace(' ', '_', $product['name']) ?>
   	<li>
   		<div class="product_wrapper">
   			<div class="product_tnail">
           <img src="<?php rootDir(); ?>img/product_images/<?php echo $product['picture'] ?>">
   			</div>
   			<div class="product_main">
-  				<a href="<?php echo $row['productURL']?>"><h2><?php echo $product['name']; ?></h2></a>
+  				<a href=<<?php ABS_URL . '/public/products/graphics_cards.php' . '<?php echo $name'"><h2><?php echo $product['name']; ?></h2></a>
   				<p><?php echo $product['description']; ?></p>
   			</div>
   			<div class="product_price">
---
+++ public/products/graphics_cards.php
@@ -0,0 +1,7 @@
+<?php
+require_once ( __DIR__ . '/../config/head.php' );
+require_once ( ABS_FILE . '/php/includes/header.php' );
+require_once ( ABS_FILE . '/php/includes/splash_image_box.php' );
+require_once ( ABS_FILE . '/php/includes/article_main_outer.php' );
+
+ ?>
