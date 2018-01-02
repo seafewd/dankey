a4003eba80cfeaafb2ce4af52f50ca61e7d04891
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-02 20:36:52 +0100

    as

--- php/includes/categories.php
+++ php/includes/categories.php
@@ -12,7 +12,7 @@ Menu with nested arrays
               'id' => '',
               'subMenu' => array(
                             array(
-                              'url' => rootDir().'fillProductList.php?name=nvidia_geforce',
+                              'url' => rootDir().'php/scripts/fillProductList.php?name=nvidia_geforce',
                               'text' => 'Nvidia Geforce',
                               'class' => 'gfx_nvidia',
                               'subMenu' => ''
