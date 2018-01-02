67e0158df4bb894d5297a466a79dc6aca9923650
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-02 20:57:46 +0100

    ^dsf

--- public/product_list.php
+++ public/product_list.php
@@ -2,11 +2,12 @@
 //page title
 $title = 'Products';
 $para = $_GET['name'];
+echo $para;
 require_once ( __DIR__ . '/../config/head.php' );
 require_once ( ABS_FILE . '/php/includes/header.php' );
 require_once ( ABS_FILE . '/php/includes/splash_image_box.php' );
 require_once ( ABS_FILE . '/php/includes/article_main_outer.php' );
-require_once ( ABS_FILE . '/php/scripts/fillProductList.php?name=' . $para);
+require_once ( ABS_FILE . '/php/scripts/fillProductList.php');
 ?>
 
 <link rel="stylesheet" href="<?php rootDir(); ?>css/product_list.css">
