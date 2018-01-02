c60673a9499b46e142e1762c2b52b75f39a7ad2f
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-02 22:27:47 +0100

    asdf

--- public/products/graphics_cards.php
+++ public/products/graphics_cards.php
@@ -1,5 +1,5 @@
 <?php
-require_once ( __DIR__ . '/../config/head.php' );
+require_once ( __DIR__ . '/../../config/head.php' );
 require_once ( ABS_FILE . '/php/includes/header.php' );
 require_once ( ABS_FILE . '/php/includes/splash_image_box.php' );
 require_once ( ABS_FILE . '/php/includes/article_main_outer.php' );
