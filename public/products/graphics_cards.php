bf203fb065cb0bba58b1694f2003fc441f3411cc
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-02 22:26:04 +0100

    asdasd

--- public/products/graphics_cards.php
+++ public/products/graphics_cards.php
@@ -4,4 +4,18 @@ require_once ( ABS_FILE . '/php/includes/header.php' );
 require_once ( ABS_FILE . '/php/includes/splash_image_box.php' );
 require_once ( ABS_FILE . '/php/includes/article_main_outer.php' );
 
+$name = str_replace('_', ' ', $_GET['produdct']);
+
+$pdo = new PDO('mysql:host=localhost;dbname=dankeyswebshop', 'dankey', 'J2DGi7Ql#XG&u^');
+$statement = $pdo->prepare("SELECT * FROM graphics_cards WHERE name = :name");
+$result = $statement->execute(array('name'=>$name));
+$product = $statement->fetch();
+
+echo $product['name'];
+echo $product['price'];
+echo $product['storage'];
+echo $product['width'];
+echo $product['height'];
+echo $product['length'];
+
  ?>
