3ce6c4a551f043bfdc6de0f9a2a75bb057cf573b
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-02 20:40:52 +0100

    asd

--- public/product_list.php
+++ public/product_list.php
@@ -5,7 +5,6 @@ require_once ( __DIR__ . '/../config/head.php' );
 require_once ( ABS_FILE . '/php/includes/header.php' );
 require_once ( ABS_FILE . '/php/includes/splash_image_box.php' );
 require_once ( ABS_FILE . '/php/includes/article_main_outer.php' );
-
 require_once ( ABS_FILE . '/php/scripts/fillProductList.php');
 ?>
 
