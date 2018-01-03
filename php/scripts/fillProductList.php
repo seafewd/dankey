d27411b57f7cf4122305176dc5aafb01a28c3715
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-03 13:42:40 +0100

    asd

--- php/scripts/fillProductList.php
+++ php/scripts/fillProductList.php
@@ -28,7 +28,7 @@ foreach ($category as $cat) {
           <img src="<?php rootDir(); ?>img/product_images/<?php echo $product['picture'] ?>">
         </div>
         <div class="product_main">
-          <?php echo '<a href="' . ABS_URL . 'public/products/' . $cat . '.php?product=' . $name . '"><h2>product['name']</h2></a>'; ?>
+          <?php echo '<a href="' . ABS_URL . 'public/products/' . $cat . '.php?product=' . $name . '"><h2> $product['name'] </h2></a>'; ?>
           <!--<a href="http://dankeytec.internet-box.ch/public/products/graphics_cards.php?product=<?php// echo $name ?>"><h2><?php// echo $product['name']; ?></h2></a>-->
         </div>
         <div class="product_price">
