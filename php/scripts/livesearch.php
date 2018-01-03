0fdf2c6b54248eccf034c34d53465d10e7863ab6
Alexander Korpas <alexander@alexanders-macbook-air.home>
2018-01-03 11:52:34 +0100

    bla

--- php/scripts/livesearch.php
+++ php/scripts/livesearch.php
@@ -1,27 +1,32 @@
 <?php
+require_once ( __DIR__ . '/functions.php');
 
 try{
     $pdo = new PDO("mysql:host=localhost;dbname=dankeyswebshop", "dankey", "xyz");
     // Set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
     die("ERROR: Could not connect. " . $e->getMessage());
 }
 
+showAlert('test');
+
+
 // Attempt search query execution
 try{
     if(isset($_REQUEST['term'])){
+        showAlert($_REQUEST['term']);
         // create prepared statement
-        $sql = "SELECT * FROM products WHERE name LIKE :term";
-        $stmt = $pdo->prepare($sql);
+        $sql = "SELECT * FROM graphics_cards WHERE name LIKE :term";
+        $stmt = $pdo -> prepare($sql);
         $term = $_REQUEST['term'] . '%';
         // bind parameters to statement
-        $stmt->bindParam(':term', $term);
+        $stmt -> bindParam(':term', $term);
         // execute the prepared statement
-        $stmt->execute();
-        if($stmt->rowCount() > 0){
-          echo "hello";
-            while($row = $stmt->fetch()){
+        $stmt -> execute();
+        if($stmt -> rowCount() > 0){
+          showAlert('rows: ' . rowCount());
+            while($row = $stmt -> fetch()){
               echo "<p>" . $row['name'] . "</p>";
             }
         } else{
