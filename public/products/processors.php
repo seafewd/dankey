7eb0d6df6e1cad7f456aaafe3a02138d79fd564f
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-03 13:24:19 +0100

    as

---
+++ public/products/processors.php
@@ -0,0 +1,21 @@
+<?php
+require_once ( __DIR__ . '/../../config/head.php' );
+require_once ( ABS_FILE . '/php/includes/header.php' );
+require_once ( ABS_FILE . '/php/includes/splash_image_box.php' );
+require_once ( ABS_FILE . '/php/includes/article_main_outer.php' );
+
+$name = str_replace('_', ' ', $_GET['product']);
+
+echo "<script type='text/javascript'>alert('$name');</script>";
+
+$pdo = new PDO('mysql:host=localhost;dbname=dankeyswebshop', 'dankey', 'J2DGi7Ql#XG&u^');
+$statement = $pdo->prepare("SELECT * FROM graphics_cards WHERE name = :name");
+$result = $statement->execute(array('name'=>$name));
+$product = $statement->fetch();
+
+echo '<p>' . $product['name']. "</p>\n";
+echo '<p>' . $product['price']. "</p>\n";
+echo '<p>' . $product['clock_frequency']. "</p>\n";
+echo '<p>' . $product['']. "</p>\n";
+
+ ?>
