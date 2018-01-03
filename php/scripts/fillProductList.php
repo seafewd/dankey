e623a4aa0b0a8a0f99c837eb29886ad60c76ad22
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-03 02:35:34 +0100

    asd

--- php/scripts/fillProductList.php
+++ php/scripts/fillProductList.php
@@ -6,13 +6,11 @@ require_once ( ABS_FILE . '/php/classes/db.php');
 //require_once ( ABS_URL . 'php/classes/db.php');
 $para = $_GET['name'];
 
-echo $para;
-
 $pdo = new PDO('mysql:host=localhost;dbname=dankeyswebshop', 'dankey', 'J2DGi7Ql#XG&u^');
 $statement = $pdo->prepare("SELECT DISTINCT category FROM products WHERE subcategory = :subcategory");
 $statement->bindParam(1, $para, PDO::PARAM_STR);
 $result = $statement->execute();
-$category = $statement->fetch();
+$category = $statement->fetchAll();
 
 echo $category;
 
