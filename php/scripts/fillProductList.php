6a83dcb62e10d48a1b601272c113e77465979151
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-03 14:20:00 +0100

    ads

--- php/scripts/fillProductList.php
+++ php/scripts/fillProductList.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once ( __DIR__ . '/functions.php');
 
 $para = $_GET['name'];
--- public/products/processors.php
+++ public/products/processors.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 require_once ( __DIR__ . '/../../config/head.php' );
 require_once ( ABS_FILE . '/php/includes/header.php' );
 require_once ( ABS_FILE . '/php/includes/splash_image_box.php' );
