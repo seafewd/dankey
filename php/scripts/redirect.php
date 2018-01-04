48282ca085a3e9cc5f10dc223ce692ce8668bb26
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-04 15:07:54 +0100

    asd

--- php/scripts/redirect.php
+++ php/scripts/redirect.php
@@ -4,17 +4,17 @@ $name = str_replace('+', ' ', $_GET['search_text']);
 $pdo = new PDO('mysql:host=localhost;dbname=dankeyswebshop', 'dankey', 'J2DGi7Ql#XG&u^');
 $statement = $pdo->prepare("SELECT subcategory FROM processors WHERE name LIKE :name UNION SELECT subcategory FROM memory  WHERE name LIKE :name UNION SELECT subcategory FROM graphics_cards WHERE name LIKE :name");
 $result = $statement->execute(array('name'=>$name));
-$category = $statement->fetchAll(PDO::FETCH_COLUMN);
+$subcategory = $statement->fetchAll(PDO::FETCH_COLUMN);
 
-foreach ($category as $cat) {
+foreach ($subcategory as $subcat) {
+  echo "subcat: " . $subcat;
   $query = "SELECT category FROM products WHERE subcategory = :subcategory";
   $statement = $pdo->prepare($query);
-  $result = $statement->execute(array('subcategory'=>$cat));
-  $finalcategory = $statement->fetchAll();
+  $result = $statement->execute(array('subcategory'=>$subcat));
+  $category = $statement->fetchAll();
 
-  foreach ($finalcategory as $carlo) {
-    echo $carlo;
-    echo "called";
+  foreach ($category as $cat) {
+    echo "category: " . $cat;
   }
 
 };
