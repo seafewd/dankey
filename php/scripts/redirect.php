ce4744fc516346b7cc44f887cbc581836ceefc55
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-04 15:00:45 +0100

    asd

--- php/scripts/redirect.php
+++ php/scripts/redirect.php
@@ -2,5 +2,21 @@
 $name = str_replace('+', ' ', $_GET['search_text']);
 echo $name;
 
+$para = $name;
+
+$pdo = new PDO('mysql:host=localhost;dbname=dankeyswebshop', 'dankey', 'J2DGi7Ql#XG&u^');
+$statement = $pdo->prepare("SELECT subcategory FROM processors WHERE name LIKE :name UNION SELECT subcategory FROM memory  WHERE name LIKE :name UNION SELECT subcategory FROM graphics_cards WHERE name LIKE :name");
+$result = $statement->execute(array('name'=>$name));
+$category = $statement->fetchAll(PDO::FETCH_COLUMN);
+
+echo $category
+
+/*foreach ($category as $cat) {
+  $tmp = $cat;
+  $query = "SELECT * FROM $cat WHERE subcategory = :subcategory";
+  $statement = $pdo->prepare($query);
+  $result = $statement->execute(array('subcategory'=>$para));
+  $product_list = $statement->fetchAll();
+};*/
 
 ?>
