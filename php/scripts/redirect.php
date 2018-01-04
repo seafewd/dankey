10ca135eb83ad79f9ac429e0ceedf21b53de7168
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-04 15:10:56 +0100

    asd

--- php/scripts/redirect.php
+++ php/scripts/redirect.php
@@ -11,7 +11,7 @@ foreach ($subcategory as $subcat) {
   $query = "SELECT category FROM products WHERE subcategory LIKE :subcategory";
   $newstatement = $pdo->prepare($query);
   $result = $newstatement->execute(array('subcategory'=>$subcat));
-  $category = $newstatement->fetch();
+  $category = $newstatement->fetchAll(PDO::FETCH_COLUMN);
 
   foreach ($category as $cat) {
     echo " category: " . $cat;
