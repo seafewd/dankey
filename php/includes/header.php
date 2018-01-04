3df8695ea9793a6eba4d4b1cbb67f970bab7cd27
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-04 12:42:46 +0100

    df

--- php/includes/header.php
+++ php/includes/header.php
@@ -19,7 +19,8 @@ require_once ( __DIR__ . '/../scripts/functions.php');
         <h1 class="companyHeader"><a href="<?php rootDir();?>index.php"><span class="highlight">Dankey's</span> TecShop</a></h1>
         <div id="searchBar">
         <form>
-          <input type="email" placeholder="Search for a product...">
+          <input type="text" name="search_text" id="search_text" placeholder="Search for a product..." autocomplete="off"/>
+          <div id="search_result"></div>
           <button type="submit" class="searchBar_button">Search</button>
         </form>
       </div>
