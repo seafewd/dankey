1f22bdd72840d4c6d16e715d31578684e830ac12
Korpas Alexander Rikard <alexander.korpas@students.bfh.ch>
2018-01-04 12:11:49 +0100

    added disqus

--- public/product_view.php
+++ public/product_view.php
@@ -27,19 +27,23 @@ require_once(ABS_FILE . '/php/includes/article_main_outer.php');
 
     <div id="product-top">
         <div id="product-gallery">
-            <img src="<?php rootDir(); ?>img/ASUS_GEFORCE_GTX_1070_STRIX_O8G.png"/>
+            <div id="product-gallery-inner">
+                <img src="<?php rootDir(); ?>img/ASUS_GEFORCE_GTX_1070_STRIX_O8G.png"/>
+            </div>
         </div>
 
         <div id="product-info">
+          <div id="product-info-inner">
             <h2 class="product-price">518.-</h2>
             <form class="addToBasket_form">
                 <input type="button" name="addToBasket" action="POST" value="Add to cart"/>
             </form>
+          </div>
         </div>
     </div>
 
-
     <div id="product-specifications">
+      <h2>Product specifications</h2>
         <table class="product-spec">
             <tr>
                 <th>General</th>
@@ -104,5 +108,31 @@ require_once(ABS_FILE . '/php/includes/article_main_outer.php');
     </div>
 
 </article>
+
+<div class="line_separator"></div>
+
+<section id="disqus">
+    <div id="disqus_thread"></div>
+    <script>
+
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+    /*
+    var disqus_config = function () {
+    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    */
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://dankeytec.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
+</section>
 
 <?php require_once(ABS_FILE . '/php/includes/footer.php'); ?>
