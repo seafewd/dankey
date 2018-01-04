ddb222d11a0a30ec36f4c5c9ffd93eb2b6e32ec7
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-04 12:35:16 +0100

    dsa

---
+++ js/livesearch.js
@@ -0,0 +1,45 @@
+//AJAX script for live search
+var root = document.location.hostname;
+var noMatchesMsg = "No matches found.";
+
+$(document).ready(function(){
+    //hide/show search result depending on where the user clicks
+    $(document).click(function(event){
+        if ($(event.target).attr('id') !== 'search_text' || $('#searchBar form input[type="text"]').val().length === 0) {
+            $("#search_result").fadeOut();
+
+        } else
+            $("#search_result").fadeIn();
+    });
+
+
+    $('#searchBar form input[type="text"]').on("keyup input", function(){
+        /* Get input value on change */
+        var inputVal = $(this).val();
+        var resultDropdown = $(this).siblings("#search_result");
+        if(inputVal.length){
+          $.get("/dankey/php/scripts/livesearch.php",{term:inputVal, noMatchesMsg:noMatchesMsg}).done(function(data){
+              // Display the returned data in browser
+              $('#search_result').css('height', 'auto');
+              $("#search_result").fadeIn();
+              resultDropdown.html(data);
+          });
+        } else{
+            $('#search_result').css('height', '0');
+            $("#search_result").fadeOut();
+            resultDropdown.empty();
+        }
+    });
+
+    // Set search input value on click of result item
+    $(document).on("click", "#search_result li", function(){
+      if ($('#search_result li').text() != noMatchesMsg) {
+        $(this).parents("#searchBar").find('input[type="text"]').val($(this).text());
+
+      }
+      $(this).parent("#search_result").empty();
+      $('#search_result').css('display', 'none');
+
+    });
+
+});
---
+++ php/scripts/livesearch.php
@@ -0,0 +1,41 @@
+<?php
+require_once(__DIR__ . '/functions.php');
+
+try {
+    $pdo = new PDO("mysql:host=localhost;dbname=dankeyswebshop", "dankey", "xyz");
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+
+// Attempt search query execution
+try {
+    // create prepared statement
+    $sql = "SELECT * FROM graphics_cards WHERE name LIKE :term";
+    $stmt = $pdo->prepare($sql);
+    $term = $_GET['term'] . '%';
+    // bind parameters to statement
+    $stmt->bindParam(':term', $term);
+    // execute the prepared statement
+    $stmt->execute();
+    if ($stmt->rowCount() > 0) {
+        echo '<ul id="search_results">';
+        while ($row = $stmt->fetch()) {
+            echo '<li>' . $row["name"] . '</li>';
+        }
+    } else {
+        echo '<li>' . $_GET['noMatchesMsg'] . '</li>';
+    }
+    echo '</ul>';
+
+} catch (PDOException $e) {
+    die("ERROR: Could not execute $sql. " . $e->getMessage());
+}
+
+
+// Close statement
+unset($stmt);
+// Close connection
+unset($pdo);
+?>
