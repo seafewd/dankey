4cdf8a9003cf04c7060c012460c81b6ed9313e53
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-05 15:32:44 +0100

    asd

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,7 +1,18 @@
 function validateForm() {
-    var x = document.forms["myForm"]["username"].value;
-    if (x == "") {
-        alert("Name must be filled out");
+    var username = document.forms["myForm"]["username"].value;
+    var email = document.forms["myForm"]["email"].value;
+    var firstname = document.forms["myForm"]["firstname"].value;
+    var lastname = document.forms["myForm"]["lastname"].value;
+    var address = document.forms["myForm"]["address"].value;
+    var city = document.forms["myForm"]["city"].value;
+    var phone = document.forms["myForm"]["phone"].value;
+    var sexOption = document.forms["myForm"]["sexOption"].value;
+    var languageOption = document.forms["myForm"]["languageOption"].value;
+    var birthday = document.forms["myForm"]["birthday"].value;
+    var password = document.forms["myForm"]["password"].value;
+
+    if (username == "") {
+        alert("please type in a valid username");
         return false;
-    }
+    }else if()
 }
