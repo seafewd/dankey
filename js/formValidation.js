82adab029f584bb3f868a697201d6b71ac2431e6
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-05 15:18:42 +0100

    asd

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,6 +1,5 @@
 function validateForm() {
-  alert("THE FUCK IS GOING ON?")
-    var x = document.forms["myForm"]["fname"].value;
+    var x = document.forms["myForm"]["name"].value;
     if (x == "") {
         alert("Name must be filled out");
         return false;
