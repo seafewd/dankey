2454fd1a68f4e4ccbeb6a088b3a570fc6a0e160e
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-06 16:52:16 +0100

    asd

--- config/head.php
+++ config/head.php
@@ -21,8 +21,6 @@ require_once( __DIR__ . '/../php/scripts/functions.php');
     <script src="<?php rootDir();?>js/sideMenu.js"></script>
     <script src="<?php rootDir();?>js/livesearch.js"></script>
     <script src="<?php rootDir();?>js/shoppingCartPopup.js"></script>
-    <script src="<?php rootDir();?>js/formValidation.js"></script>
-
 
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="57x57" href="<?php rootDir(); ?>img/favicon/apple-icon-57x57.png">
