2ce044aac2b871d73159186d4d305549c48d7493
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-06 15:53:55 +0100

    asd

--- php/scripts/fillProductList.php
+++ php/scripts/fillProductList.php
@@ -80,7 +80,7 @@ if(isSet($_GET['name'])){
       <?php }};
       if($count === 1){
         foreach ($category as $cat) {
-          $url = ABS_URL . "public/products/$cat.php?product=$para";
+          $url = '"' . ABS_URL . "public/products/$cat.php?product=$para" . '"';
           echo $url;
           ?>
           <script type="text/javascript">window.location.href = <?php echo $url ?>;</script>
