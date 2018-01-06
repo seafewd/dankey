db84153126938f5a51172635ddb9c639cb0cd7c1
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-06 22:54:59 +0100

    asd

--- index.php
+++ index.php
@@ -10,12 +10,7 @@ $pdo = new PDO('mysql:host=localhost;dbname=dankeyswebshop', 'dankey', 'J2DGi7Ql
 
 if(isSet($_SESSION['cart'])){
   $cart = $_SESSION['cart'];
-  foreach ($cart as $car) {
-    $items = $car['items'];
-    foreach ($items as $item) {
-      echo $item->getName();
-    } // End of foreach loop!
-  }
+  echo "This cart is empty: " + $cart->isEmpty();
 }
 
 if(isSet($_POST["login"])){
