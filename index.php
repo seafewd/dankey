4615d2670ebe6e50b3d9067bf6aa4cca6e0520e2
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-06 23:21:06 +0100

    asd

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $newCart = unserialize($_SESSION['cart']);
 
 echo '<h2>Cart Contents newCart (' . count($newCart) . ' items)</h2>';
 if (!$newCart->isEmpty()) {
-    foreach ($$newCart as $arr) {
+    foreach ($newCart as $arr) {
         $item = $arr['item'];
         printf('<p><strong>%s</strong>: %d @ $%0.2f each.<p>', $item->getName(), $arr['qty'], $item->getPrice());
     } // End of foreach loop!
