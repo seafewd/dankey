1e250298e2a701eccad7d7571d6ed9891e0b3bc2
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-06 23:13:37 +0100

    asd

--- php/scripts/shopping.php
+++ php/scripts/shopping.php
@@ -5,7 +5,7 @@ require_once ( ABS_FILE . '/php/classes/Item.php');
 
 session_start();
 
-if(isSet($_SESSION['cart'])){
+/*if(isSet($_SESSION['cart'])){
   $cart = $_SESSION['cart'];
 }else{
   $cart = new ShoppingCart();
@@ -16,6 +16,18 @@ $name = str_replace('%20', ' ', $_GET['name']);
 $price = $_GET['price'];
 
 $item = new Item($name, $name, $price);
-$_SESSION['test'] = $item;
+$_SESSION['test'] = $item;*/
+
+$cart = new ShoppingCart();
+
+$w1 = new Item('W139', 'Some Widget', 23.45);
+$w2 = new Item('W384', 'Another Widget', 12.39);
+$w3 = new Item('W55', 'Cheap Widget', 5.00);
+
+$cart->addItem($w1);
+$cart->addItem($w2);
+$cart->addItem($w3);
+
+$_SESSION['cart'] = $cart;
 
 ?>
