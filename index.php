5f69be83de2bda835c691a556d605a42d79d8896
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-06 23:06:49 +0100

    asd

--- index.php
+++ index.php
@@ -8,10 +8,26 @@ require_once ( ABS_FILE . '/php/classes/Item.php');
 //start login script
 $pdo = new PDO('mysql:host=localhost;dbname=dankeyswebshop', 'dankey', 'J2DGi7Ql#XG&u^');
 
-if(isSet($_SESSION['test'])){
-  $item = $_SESSION['test'];
-  echo $item->getName();
-}
+$cart = new ShoppingCart();
+
+$w1 = new Item('W139', 'Some Widget', 23.45);
+$w2 = new Item('W384', 'Another Widget', 12.39);
+$w3 = new Item('W55', 'Cheap Widget', 5.00);
+
+$cart->addItem($w1);
+$cart->addItem($w2);
+$cart->addItem($w3);
+$cart->updateItem($w2, 4);
+$cart->updateItem($w1, 1);
+$cart->deleteItem($w3);
+
+echo '<h2>Cart Contents (' . count($cart) . ' items)</h2>';
+if (!$cart->isEmpty()) {
+    foreach ($cart as $arr) {
+        $item = $arr['item'];
+        printf('<p><strong>%s</strong>: %d @ $%0.2f each.<p>', $item->getName(), $arr['qty'], $item->getPrice());
+    } // End of foreach loop!
+} // End of IF.
 
 if(isSet($_POST["login"])){
   $username = $_POST["username"];
