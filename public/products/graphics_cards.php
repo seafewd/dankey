301a9b42c8e31d376828c7a5c14af4ec4bf1d434
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-06 18:37:28 +0100

    asd

--- public/products/graphics_cards.php
+++ public/products/graphics_cards.php
@@ -4,7 +4,6 @@ session_start();
 require_once ( __DIR__ . '/../../config/head.php' );
 require_once ( ABS_FILE . '/php/includes/header.php' );
 require_once ( ABS_FILE . '/php/includes/article_main_outer.php' );
-require_once ( ABS_FILE . '/php/scripts/shopping.php' );
 
 $name = str_replace('_', ' ', $_GET['product']);
 
