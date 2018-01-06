817ba2b23ff9a9cd9c4840304ab1bd2b030a6cc1
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-06 22:18:37 +0100

    asd

--- php/scripts/shopping.php
+++ php/scripts/shopping.php
@@ -1,9 +1,9 @@
 <?php
-session_start();
-
 require_once ( __DIR__ . '/../../config/head.php' );
 require_once ( ABS_FILE . '/php/classes/ShoppingCart.php');
 require_once ( ABS_FILE . '/php/classes/Item.php');
+
+session_start();
 
 if(isSet($_SESSION['cart'])){
   $cart = $_SESSION['cart'];
