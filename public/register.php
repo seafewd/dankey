451203e62ab3194ff88c13f7b9adeb1bc8cafbed
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-06 16:27:31 +0100

    asd

--- public/register.php
+++ public/register.php
@@ -24,8 +24,11 @@ if(false) {
   $birthday = $_POST['birthday'];
   $sex = $_POST['sexOption'];
 
-  $db -> doQuery("INSERT INTO users (username, email, password, firstname, lastname, address, city, phone, language, birthday, sex) VALUES
+  $pdo = new PDO('mysql:host=localhost;dbname=dankeyswebshop', 'dankey', 'J2DGi7Ql#XG&u^');
+  $statement = $pdo->prepare("INSERT INTO users (username, email, password, firstname, lastname, address, city, phone, language, birthday, sex) VALUES
   ('$username', '$email', '$password', '$firstname', '$lastname', '$address', '$city', '$phone', '$language', '$birthday', '$sex')");
+  $statement->execute();
+
 }
 ?>
 <script>
