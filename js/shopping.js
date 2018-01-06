30bcf254b5a9eb2f9ba7a8ff94f9b5780b18c84c
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-06 19:16:05 +0100

    asd

--- js/shopping.js
+++ js/shopping.js
@@ -1,3 +1,9 @@
 function addToCart(){
-  addToCartPHP();
+  echo "<script type='text/javascript'>alert('called that shit');</script>";
+  if (isset($_SESSION['cart'])) {
+    $cart = $_SESSION['cart'];
+  } else {
+    echo "<script type='text/javascript'>alert('cart created');</script>";
+    $cart = new ShoppingCart();
+  }
 }
