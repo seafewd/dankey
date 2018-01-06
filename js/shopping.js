8fbff7ad0bfe937cf37fe802920d83f3b83a5e49
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-06 19:03:14 +0100

    asd

--- js/shopping.js
+++ js/shopping.js
@@ -1,27 +1,3 @@
-/*<?php
-require_once ( __DIR__ . '/../../config/head.php' );
-require_once ( ABS_FILE . '/php/classes/ShoppingCart.php');
-require_once ( ABS_FILE . '/php/classes/Item.php');
-
-function addToCart($name, $price) {
-  echo "<script type='text/javascript'>alert('called that shit');</script>";
-  if (isset($_SESSION['cart'])) {
-    $cart = $_SESSION['cart'];
-  } else {
-    $cart = new ShoppingCart();
-  }
-  $cart->addItem(new Item($name, $name, $price));
-  foreach ($cart as $arr) {
-        $item = $arr['item'];
-        printf('<p><strong>%s</strong>: %d @ $%0.2f each.<p>', $item->getName(), $arr['qty'], $item->getPrice());
-    }
-}
-?>*/
 function addToCart(){
-  if (isset($_SESSION['cart'])) {
-    $cart = $_SESSION['cart'];
-  } else {
-    alert("cart created");
-    $cart = new ShoppingCart();
-  }
+  addToCartPHP();
 }
