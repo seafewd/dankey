71d069581a1194d3491e5081e13f71f516fbfc4e
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-06 20:26:18 +0100

    asd

--- php/scripts/shopping.php
+++ php/scripts/shopping.php
@@ -3,7 +3,6 @@ require_once ( __DIR__ . '/../../config/head.php' );
 require_once ( ABS_FILE . '/php/classes/ShoppingCart.php');
 require_once ( ABS_FILE . '/php/classes/Item.php');
 
-echo $_GET['name'];
-echo $_GET['price'];
+    echo "<script type='text/javascript'>alert('hello there');</script>";
 
 ?>
