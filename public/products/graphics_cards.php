2e77218676e1d95c9f3fdac776a67264cb90ca24
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-06 21:52:05 +0100

    hjk

--- public/products/graphics_cards.php
+++ public/products/graphics_cards.php
@@ -53,7 +53,7 @@ function addToCart(name, price){
           <div id="product-info-inner">
             <h2 class="product-price"><?php echo $product['price']; ?>.-</h2>
             <form class="addToBasket_form">
-              <?php echo '<input type="button" name="addToBasket" method="post" value="Add to cart" onclick="addToCart(' . $product['name'] . ',' . $product['price'] .')"/>'; ?>
+              <?php echo '<input type="button" name="addToBasket" method="post" value="Add to cart" onclick="addToCart('' . $product['name'] . ''',' . $product['price'] .')"/>'; ?>
             </form>
           </div>
         </div>
