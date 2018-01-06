1b18ca39f8f989a8ec07d2207fb52c27e0e7df54
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-06 15:04:39 +0100

    asd

--- php/scripts/fillProductList.php
+++ php/scripts/fillProductList.php
@@ -50,9 +50,6 @@ if (sizeOf($product_list) === 0) {
   $statement->bindParam(':name', $term);
   $statement->execute();
 
-  
-
-
   while ($row = $statement->fetch()) {
     $newstatement = $pdo->prepare("SELECT DISTINCT category FROM products WHERE subcategory = :subcategory");
     $result = $newstatement->execute(array('subcategory'=>$row['subcategory']));
