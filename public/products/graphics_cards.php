3a2eeeab50dd923a055e584ad1e38a337d9a9844
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-06 21:32:09 +0100

    asd

--- public/products/graphics_cards.php
+++ public/products/graphics_cards.php
@@ -56,7 +56,6 @@ function addToCart(name, price){
             <h2 class="product-price"><?php echo $product['price']; ?>.-</h2>
             <form class="addToBasket_form">
               <?php echo '<input type="button" name="addToBasket" method="post" value="Add to cart" onclick="addToCart(' . $name . ',' . $price .')"/>'; ?>
-                <!--<input type="button" name="addToBasket" method="post" value="Add to cart" onclick="addToCart('ASUS_GeForce_GTX_1070_STRIX_O8G-GAMING', '515')"/>-->
             </form>
           </div>
         </div>
