29b267aea23c4961593a6b3ce450f32811d8bb3a
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-06 19:33:28 +0100

    asd

--- js/shopping.js
+++ js/shopping.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
   function addToCart() {
-    var cart = getCookie("cart");
+    var cart = read_cookie("cart");
     if(cart != ""){
       alert("there is already a cart");
     }else{
       alert("there is no cart yet");
       document.cookie = "cart=hello";
-
     }
 
   }
