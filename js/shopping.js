0fa34d372b08d020c4eeb1feadef1bb9add1df86
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-06 19:40:39 +0100

    asd

--- js/shopping.js
+++ js/shopping.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
   function addToCart() {
+    var cart = new ShoppingCart();
+    document.cookie = cart;
     alert("called that cancer bullshit crap ficken");
   }
   $("#addToBasket").click(addToCart);
