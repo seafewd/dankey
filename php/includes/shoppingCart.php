17b1cc757dfff49ae6586b703c80f28141416b3d
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-07 03:10:42 +0100

    as

--- php/includes/shoppingCart.php
+++ php/includes/shoppingCart.php
@@ -33,7 +33,7 @@ if(isset($_SESSION['cart'])){
       <div class="cart-item">
         <p>
           <?php echo $arr['qty']?>x
-          <a href="<?php echo ABS_URL . '/public/products/' . $pdo->getCategoryByProduct($item->getName()) . '.php?product=' . $item->getName() ?>"><?php echo $item->getName() ?></a><br/>
+          <a href="<?php echo ABS_URL . 'public/products/' . echo $pdo->getCategoryByProduct($item->getName()) . '.php?product=' . echo $item->getName() ?>"><?php echo $item->getName() ?></a><br/>
           <span class="price"><?php echo $item->getPrice()?>CHF</span>
         </p>
       </div>
