59a0da92d1e5c12cfd28f71a5a63d8e6cb961326
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-07 02:43:35 +0100

    asd

--- php/scripts/fillProductList.php
+++ php/scripts/fillProductList.php
@@ -8,7 +8,12 @@ $pdo = DB::getInstance();
 if(isSet($_GET['name'])){
 $para = $_GET['name'];
 
-$cat = DB::getProductCategory($para);
+$statement = $pdo->db->prepare("SELECT DISTINCT category FROM products WHERE subcategory = :subcategory");
+$result = $statement->execute(array('subcategory'=>$para));
+$category = $statement->fetchAll(PDO::FETCH_COLUMN);
+
+foreach ($category as $cat) {
+  $tmp = $cat;
   $query = "SELECT * FROM $cat WHERE subcategory = :subcategory";
   $statement = $pdo->db->prepare($query);
   $result = $statement->execute(array('subcategory'=>$para));
