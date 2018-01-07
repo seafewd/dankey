92477bc2e08eaf42f854664f724e5cc5035f43cd
Korpas Alexander Rikard <alexander.korpas@students.bfh.ch>
2018-01-07 18:05:45 +0100

    added stuff yknow

--- public/product_list.php
+++ public/product_list.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+
 //page title
 $title = 'Products';
 
@@ -9,6 +10,18 @@ require_once ( ABS_FILE . '/php/includes/article_main_outer.php' );
 require_once ( ABS_FILE . '/php/scripts/fillProductList.php');
 ?>
 
+<script>
+$(document).ready(function() {
+    $('.product_main').hover(function() {
+        $(this).siblings('.product_tnail').css('border-color', 'transparent', 'border-right', '1px solid lightgray', 'border-radius', '0');
+    }, function() {
+        $('.product_tnail').css('border-color', 'lightgray', 'border-radius', '20px');
+    });
+});
+</script>
+
 <link rel="stylesheet" href="<?php rootDir(); ?>css/product_list.css">
+
+
 
 <?php require_once( ABS_FILE . '/php/includes/footer.php'); ?>
