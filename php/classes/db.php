643826791bc523ba89c6a023efc7580c53a47efb
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-07 01:40:29 +0100

    asd

--- php/classes/db.php
+++ php/classes/db.php
@@ -10,7 +10,7 @@ class DB{
   private static $instance;
 
   private function __construct(){
-    $this->db = new PDO(HOST;DBNAME,USER,PW);
+    $this->db = new PDO(HOST.DBNAME,USER,PW);
   }
 
   public static function getInstance(){
