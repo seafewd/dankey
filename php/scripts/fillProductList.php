bd4e189bf9f1db348c71fb0d3da734d20c489ab8
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-07 01:34:20 +0100

    asd

--- php/scripts/fillProductList.php
+++ php/scripts/fillProductList.php
@@ -1,7 +1,7 @@
+<<<<<<< current
 <?php
-require_once ( __DIR__ . '/../config/head.php' );
+require_once ( __DIR__ . '/../../config/head.php' );
 require_once ( ABS_FILE . '/php/classes/DB.php');
-
 $pdo = DB::getInstance();
 
 //is set if user comes from sidemenu choice
@@ -85,3 +85,40 @@ if (sizeOf($product_list) === 0) {
           <script type="text/javascript">window.location.href = <?php echo $url ?>;</script>
         <?php }}}; ?>
     </ul>
+=======
+<?php
+require_once ( __DIR__ . '/functions.php');
+
+//this script is used to query the DB and get all products for this category
+require_once ( ABS_FILE . '/php/classes/db.php');
+//require_once ( ABS_URL . 'php/classes/db.php');
+$para = $_GET['name'];
+
+$pdo = new PDO('mysql:host=localhost;dbname=dankeyswebshop', 'dankey', 'J2DGi7Ql#XG&u^');
+$statement = $pdo->prepare("SELECT * FROM products WHERE subcategory = :subcategory");
+$result = $statement->execute(array('subcategory'=>$para));
+$product_list = $statement->fetchAll();
+
+?>
+
+<ul id="productList">
+<?php foreach($product_list as $product) { ?>
+  <?php $name = str_replace(' ', '_', $product["name"]) ?>
+  	<li>
+  		<div class="product_wrapper">
+  			<div class="product_tnail">
+          <img src="<?php rootDir(); ?>img/product_images/<?php echo $product['picture'] ?>">
+  			</div>
+  			<div class="product_main">
+  				<a href="http://dankeytec.internet-box.ch/public/products/graphics_cards.php?product=<?php echo $name ?>"><h2><?php echo $product['name']; ?></h2></a>
+  			</div>
+  			<div class="product_price">
+  				<h3><?php echo $product['price']; ?>.-</h3>
+  			</div>
+  		</div>
+  	</a>
+  </li>
+<?php } ?>
+
+</ul>
+>>>>>>> before discard
