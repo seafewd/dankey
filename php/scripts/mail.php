e01c1a06bca47c1a7c4fa96a24522708eadf83f2
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-08 23:49:57 +0100

    made mail cool

--- php/scripts/mail.php
+++ php/scripts/mail.php
@@ -1,10 +1,24 @@
 <?php
 
 function sendConfirmation(){
+  $cart = unserialize($_SESSION['cart'])
+
   $reciever = $_SESSION['user'];
   $subject = "Your Order on DankeyTec!";
   $from = "From: Dankey's TecShop <no-reply@dankeytec.ch>";
-  $text = "You're order has been placed. Thank you, mate! PLEASE DONT RESPOND TO THAT EMAIL";
+  $intro = "You're order has been placed. Thank you, mate!\n\nOrder Overview \n\n";
+  $disclaimer = "PLEASE DONT RESPOND TO THAT EMAIL";
+  $order = "";
+  $totalprice = 0;
+
+  foreach ($cart as $arr) {
+    $item = $arr['item'];
+    $order = $order . $item->getName() . "\t" . $arr['qty'] . "\t" . $item->getPrice() . "\n";
+    $totalprice += $item->getPrice() * $arr['qty'];
+  }
+
+  $text = $intro . $order . "\n Total price \t\t" . $totalprice . "\n\n" . $disclaimer;
+
   //sending
   mail($reciever, $subject, $text, $from);
 
