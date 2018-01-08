65b6e00818d1aad0feb43dc0a4982ae4bb9adfba
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-08 22:19:06 +0100

    fuicking with redirect.ph

--- php/scripts/redirect.php
+++ php/scripts/redirect.php
@@ -5,15 +5,6 @@ require_once ( ABS_FILE . '/php/classes/db.php');
 $name = str_replace('+', ' ', $_GET['search_text']);
 $redirect = str_replace(' ', '_', $name);
 
-$pdo = DB::getInstance();
-
-$statement = $pdo->db->prepare("SELECT subcategory FROM processors WHERE name LIKE :name UNION SELECT subcategory FROM memory  WHERE name LIKE :name UNION SELECT subcategory FROM graphics_cards WHERE name LIKE :name");
-$term = '%' . $name . '%';
-$statement->bindParam(':name', $term);
-$statement->execute();
-
-while ($row = $statement->fetch()) {
   header("Location:" . ABS_URL . "public/product_list.php?search_text=$redirect");
   exit;
-};
 ?>
