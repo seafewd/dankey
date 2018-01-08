a8ec1217b759e29a13fa93cbf41e2226eb373c73
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-08 23:24:49 +0100

    mail

--- public/mail.php
+++ public/mail.php
@@ -1,10 +1,16 @@
 <?php
 //confirmation to client
-$reciever = 'patrick.werlen@hotmail.com';
+$reciever = $_SESSION['user'];
 $subject = "Your Order on DankeyTec!";
-$from = "From: Nils Reimers <noidea@dankeytec.ch>";
-$text = "You're order has been placed. Thank you, mate!";
+$from = "From: Dankey's TecShop <no-reply@dankeytec.ch>";
+$text = "You're order has been placed. Thank you, mate! PLEASE DONT RESPOND TO THAT EMAIL";
 //sending
 mail($reciever, $subject, $text, $from);
+
+//confirmation to office
+$reciever = 'patrick.werlen@students.bfh.ch';
+$subject = "NEW ORDER";
+$from = "From: newOrder <order@dankeytec.ch>";
+$text = "Client X ordered bullshit!";
 
 ?>
