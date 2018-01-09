44664733e4cd02c42547df6ce5d0ac84489ce18d
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-09 13:39:32 +0100

    asd

---
+++ error.php
@@ -0,0 +1,3 @@
+<?php require_once ( __DIR__ . '/config/head.php' ); ?>
+
+<iframe src="https://giphy.com/embed/njOvnQUX3H1v2" width="457" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/e3-statue-donkey-njOvnQUX3H1v2">via GIPHY</a></p>
