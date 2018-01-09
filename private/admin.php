453ecc0bf7ad30a88432d60dbf22d63a4dcb1f47
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-09 15:41:10 +0100

    ad

--- private/admin.php
+++ private/admin.php
@@ -8,6 +8,10 @@ $user_list = $pdo->getAllUsers();
 
 <h1>This is the admin page</h1>
 <h3>All users</h3>
-<?php foreach ($user_list as $users) {
-  echo "<p>"$users['username'] . $users['firstname'] . $users['lastname'] . $users['address'] . $users['city']" </p><br>";
-}?>
+<?php foreach ($user_list as $users) { ?>
+  <p><?php echo $users['username']?></p>
+  <p><?php echo $users['firstname']?></p>
+  <p><?php echo $users['lastname']?></p>
+  <p><?php echo $users['address']?></p>
+  <p><?php echo $users['city']?></p><br>
+<?php }?>
