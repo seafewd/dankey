a2825d903ad94b49966cc6856764223fca215567
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-09 13:06:46 +0100

    asd

--- public/login.php
+++ public/login.php
@@ -1,5 +1,5 @@
 <?php
-require_once ( __DIR__ . '/../config/head.php' );
+require_once ( __DIR__ . '/../php/scripts/functions.php' );
  ?>
 
 <form id="login" action="<?php rootDir() ?>index.php"; method="post" accept-charset="UTF-8">
