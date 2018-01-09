ac4c12bd1685a250d43010a5f5857b5bda26d592
Korpas Alexander Rikard <alexander.korpas@students.bfh.ch>
2018-01-09 15:02:39 +0100

    JAJAJAJ

--- public/register.php
+++ public/register.php
@@ -9,78 +9,74 @@ require_once ( ABS_FILE . '/php/classes/db.php');
 
 //todo make this work again
 if(isSet($_POST['submit'])) {
-  $username = $_POST['username'];
-  $email = $_POST['email'];
-  $password = $_POST['password'];
-  $firstname = $_POST['firstname'];
-  $lastname = $_POST['lastname'];
-  $address = $_POST['address'];
-  $city = $_POST['city'];
-  $phone = $_POST['phone'];
-  $language = $_POST['languageOption'];
-  $birthday = $_POST['birthday'];
-  $sex = $_POST['sexOption'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $address = $_POST['address'];
+    $city = $_POST['city'];
+    $phone = $_POST['phone'];
+    $language = $_POST['languageOption'];
+    $birthday = $_POST['birthday'];
+    $sex = $_POST['sexOption'];
 
-  $pdo = DB::getInstance();
+    $pdo = DB::getInstance();
 
-  $pdo->createUser($username, $email, $password, $firstname, $lastname, $address, $city, $phone, $language, $birthday, $sex);
+    $pdo = createUser($username, $email, $password, $firstname, $lastname, $address, $city, $phone, $language, $birthday, $sex);
 
 }
 ?>
 <script>
 function changeFontOpacity(value) {
-  //alert($(this).find("option:selected").attr('id'));
-  //alert($(this + "option:selected").text());
-  if ($(this + "option:selected").text() != value) {
-    $('#register #sexOption').css('opacity', '1');
-    //$('#register #languageOption').css('opacity', '1');
-  } else {
-    $('#register #sexOption').css('opacity', '0.5');
-    //$('#register #languageOption').css('opacity', '0.5');
+    //unfinished its all bullshit ignore this
+    //alert($(this).find("option:selected").attr('id'));
+    //alert($(this + "option:selected").text());
+    if ($(this + "option:selected").text() != value) {
+        $('#register #sexOption').css('opacity', '1');
+        //$('#register #languageOption').css('opacity', '1');
+    } else {
+        $('#register #sexOption').css('opacity', '0.5');
+        //$('#register #languageOption').css('opacity', '0.5');
 
-  }
+    }
 }
 </script>
+<script src="<?php rootDir();?>js/jquery.validate.js"></script>
 
 <div id="registerFormWrapper">
-  <h1>Register</h1>
-  <form id="register" action="register.php" name="myForm" method="post" accept-charset="UTF-8">
-    <input type="hidden" name="submitted" id="submitted" value="1"/>
+    <h1>Register</h1>
+    <form id="register" action="register.php" name="myForm" method="post" accept-charset="UTF-8">
+        <input type="hidden" name="submitted" id="submitted" value="1"/>
 
-    <input type="text" placeholder="Username" pattern="[0-9a-zA-ZöäüÜÄÖ_-]{3,}" title="Please choose a Username with at least 3 charachters. Allowed are nummbers, letters and the special characters - and _"name="username" id="username" maxlength="50"/><br/>
+        <input required type="text" placeholder="Username" pattern="[0-9a-zA-ZöäüÜÄÖ_-]{3,}" title="Must have a length of at least 3 characters." name="username" id="username" maxlength="50"/><br/>
+        <input required type="email" placeholder="E-mail" name="email" id="email" title="Please enter a valid e-mail address." maxlength="50"/><br/>
+        <input required type="text" placeholder="First name" pattern="[a-zA-ZöäüÜÄÖ-]{1,}" title="Please enter a valid first name" name="firstname" id="firstname" maxlength="50"/><br/>
+        <input required type="text" placeholder="Last name" pattern="[a-zA-ZöäüÜÄÖ-]{1,}" name="lastname" title="Please enter a valid last name." id="lastname" maxlength="50"/><br/>
+        <input required type="text" placeholder="Address" name="address" pattern="[a-zA-Z\söäüÜÄÖ]+[0-9]{1,}" title="Please enter a valid address. (street & number)" id="address" maxlength="50"/><br/>
+        <input required type="text" placeholder="ZIP City" pattern="[0-9]{4}[\s]{1}[a-zA-Z-\söäüÜÄÖ]+" title="Please enter a valid city. (ZIP-code & city)" name="city" id="city" maxlength="50"/><br/>
+        <input required type="tel" placeholder="Phone" name="phone" id="phone" maxlength="50"/><br/>
 
-    <input type="email" placeholder="E-mail" name="email" id="email" maxlength="50"/><br/>
+        <select id="sexOption" name="sexOption" onclick="changeFontOpacity('GenderEnglish')">
+            <option value="">Gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+        </select>
 
-    <input type="text" placeholder="First name" pattern="[a-zA-ZöäüÜÄÖ-]{1,}" title="Please enter a valid first name" name="firstname" id="firstname" maxlength="50"/><br/>
+        <select id="languageOption" name="languageOption" onclick="changeFontOpacity('Language')">
+            <option value="">Language</option>
+            <option value="deutsch">Deutsch</option>
+            <option value="english">English</option>
+        </select>
 
-    <input type="text" placeholder="Last name" pattern="[a-zA-ZöäüÜÄÖ-]{1,}" name="lastname" title="Please enter a valid last name" id="lastname" maxlength="50"/><br/>
-
-    <input type="text" placeholder="Address" name="address" pattern="[a-zA-Z\söäüÜÄÖ]+[0-9]{1,}" title="Please enter a valid address.(street&number)" id="address" maxlength="50"/><br/>
-
-    <input type="text" placeholder="ZIP City" pattern="[0-9]{4}[\s]{1}[a-zA-Z-\söäüÜÄÖ]+" title="Please enter a valid city.(ZIP&city)" name="city" id="city" maxlength="50"/><br/>
-
-    <input type="tel" placeholder="Phone" name="phone" id="phone" maxlength="50"/><br/>
-
-    <select id="sexOption" name="sexOption" onclick="changeFontOpacity('GenderEnglish')">
-      <option value="">Gender</option>
-      <option value="male">Male</option>
-      <option value="female">Female</option>
-    </select>
-
-    <select id="languageOption" name="languageOption" onclick="changeFontOpacity('Language')">
-      <option value="">Language</option>
-      <option value="deutsch">Deutsch</option>
-      <option value="english">English</option>
-    </select>
-
-    <input type="date" name="birthday" id="birthday" maxlength="50"/><br/>
-
-    <input type="password" placeholder="Password" name="password" id="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" maxlength="255"/><br/>
-
-    <input type="password" placeholder="Confirm password" name="password-conf" id="password-conf" maxlength="50"/><br/>
-
-    <input type="submit" name="submit" value="Submit"/>
-  </form>
+        <input required title="Please enter a valid date." type="date" name="birthday" id="birthday" maxlength="50"/><br/>
+        <input required type="password" placeholder="Password" name="password" id="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" maxlength="255"/><br/>
+        <input required type="password" placeholder="Confirm password" name="password-again" id="password-conf" maxlength="50" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"/><br/>
+        <input type="submit" name="submit" value="Submit"/>
+    </form>
+    <script>
+        $("#register").validate();
+    </script>
 </div>
 
 <?php require_once( ABS_FILE . '/php/includes/footer.php'); ?>
