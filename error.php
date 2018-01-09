af123c4dc933b2e4bac29b69cde20108bbed5435
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-09 13:46:12 +0100

    asd

--- error.php
+++ error.php
@@ -1,3 +1,6 @@
 <?php require_once ( __DIR__ . '/config/head.php' ); ?>
 
+<h1>OOPS, something went wrong here.</h1>
+<a href="<?php ABS_URL . 'index.php' ?>">back to site...</a>
+
 <iframe src="https://giphy.com/embed/njOvnQUX3H1v2" width="457" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/e3-statue-donkey-njOvnQUX3H1v2">via GIPHY</a></p>
