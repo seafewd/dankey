0b1e6284adaeafdf712ba6423404c609a609a7a7
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-09 14:47:51 +0100

    ad

--- private/admin.php
+++ private/admin.php
@@ -1,3 +1,9 @@
-<?php require_once ( __DIR__ . '/../config/head.php' ); ?>
+<?php
+require_once ( __DIR__ . '/../config/head.php' );
+require_once ( ABS_FILE . '/php/classes/db.php');
 
-<h1>This is the admin page<h2>
+$pdo = DB::getInstance();
+?>
+
+<h1>This is the admin page</h1>
+<h3>All users</h3>
