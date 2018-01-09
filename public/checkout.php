50265027b3d226fc143ce373097ab473190a7758
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-09 21:46:02 +0100

    asd

--- public/checkout.php
+++ public/checkout.php
@@ -46,7 +46,7 @@ $(document).ready(function() {
 </script>
 
 <script>
-document.getElementById('confirm_button').onclick = function() {
+document.getElementById('confirm').onclick = function() {
   alert("called");
   document.cookie = "order=true;;path=/";
   setTimeout(function(){
@@ -231,7 +231,7 @@ document.getElementById('confirm_button').onclick = function() {
 <div class="line_separator"></div>
 
 <div id="confirm-order">
-    <input type="submit" id="confirm_button" value="Confirm order" data-featherlight="https://dankeytec.internet-box.ch/public/confirmation.php/">
+    <input type="submit" id="confirm" value="Confirm order" data-featherlight="https://dankeytec.internet-box.ch/public/confirmation.php/">
     <p class="disclaimer">By confirming this order you are entering a binding agreement. If you don't pay us, we'll fuck your shit up.</p>
 </div>
 
