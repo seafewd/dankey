83c6c7d0a28b357c45692e1a5474701f1874abdc
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-11 15:06:28 +0100

    ad

--- js/language.js
+++ js/language.js
@@ -2,7 +2,7 @@ function getText(key){
     //read cookie
     var language = 'en';
     //get the right json file
-    file = $.getJSON('language/' + language + '.json', function(json){
+    file = $.getJSON('https://dankeytec.internet-box.ch/languages/' + language + '.json', function(json){
       document.write(json[key]);
     });
-} 
+}
