9db9cfc4536fd91360b8f0a6fbb863ce844a4575
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-11 15:49:56 +0100

    asd

--- php/scripts/languages.php
+++ php/scripts/languages.php
@@ -3,10 +3,13 @@ function t($key) {
   global $language;
   $json = file_get_contents("https://dankeytec.internet-box.ch/languages/en.json");
   $texts = json_decode($string, true);
-  if (isset($texts[$key])) {
+
+  return $texts;
+
+  /*if (isset($texts[$key])) {
     return $texts[$key];
   } else {
     return "[$key]";
-  }
+  }*/
 }
 ?>
