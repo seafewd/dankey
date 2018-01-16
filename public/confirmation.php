bbd1da3f557f8c1baacbb58183e05ed24eca8d7b
PatrickWerlen <patrick.werlen@hotmail.com>
2018-01-16 13:44:14 +0100

    ad

--- public/confirmation.php
+++ public/confirmation.php
@@ -2,5 +2,5 @@
 require_once ( __DIR__ . '/../php/scripts/functions.php' );
 ?>
 
-<h2>SUCCESSFULLY ORDERED</h2>
-<p>You're being redirected to main page</p>
+<h2><?php echo t("ordered") ?></h2>
+<p><?php echo t("redirected") ?></p>
