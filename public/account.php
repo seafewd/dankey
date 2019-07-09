659be6e760237ff70f5b83447eaa59a08884b9e7
seafewd <alex.korpas@gmail.com>
2019-07-09 08:23:33 +0200

    profilepic ajax

--- public/account.php
+++ public/account.php
@@ -25,26 +25,7 @@ $address = (isset($_SESSION["address"]) ? $_SESSION["address"] : NULL);
 $avatar = (isset($_SESSION["avatar"]) ? $_SESSION["avatar"] : NULL);
 //$ = (isset($_SESSION["avatar"]) ? $_SESSION["avatar"] : NULL);
 
-$db = DB::getInstance();
 
-
-//upload a new profile picture
-if(isSet($_POST['upload'])){
-  if( $_FILES['image']['name'] <> ""){
-    $validation = array("image/png", "image/jpeg", "image/gif");
-    if(! in_array($_FILES["image"]["type"], $validation)){
-      echo "<p>".t("picture_error")."<p>";
-    }else{
-      $temp = explode(".", $_FILES["image"]["name"]);
-      $newfilename = round(microtime(true)) . '.' . end($temp);
-      move_uploaded_file($_FILES["image"]["tmp_name"], ABS_URL.'img/avatars/'.$newfilename);
-      $_SESSION["avatar"] = $newfilename;
-      $statement = $db->db->prepare("UPDATE users SET avatar='$newfilename' WHERE id = :userid");
-      $result = $statement->execute(array('userid' => $_SESSION['userid']));
-      $user = $statement->fetch();
-    }
-  }
-}
 ?>
 
 <!-- Profile Page JS script -->
